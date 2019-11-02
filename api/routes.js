b51dde8c95c2b9b1e6dd34465f0cabe05b2ec70a
griffin <pfenniger.griffin@gmail.com>
2019-11-02 16:12:25 -0700

    endpoint outline

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,15 @@
+import express from "express";
+const router = express.Router();
+
 // TODO 3 GET endpoints for API
+
+router.get(["/faction", "/faction/:search"], (req, res) => {});
+router.get(["/article", "/article/:id"], (req, res) => {});
+router.get("/map", (req, res) => {});
 
 // TODO 2 POST endpoints for API
 
-/*
-    Implement all to be fully CRUD
-    GET
-    POST
-    UPDATE
-    DELETE
-*/
+router.post("/user", (req, res) => {});
+router.post("/article", (req, res) => {});
+
+module.exports = router;
